36f9d9d82f1bdab38fab2a9ec3fad0aca2f37afe
nk125 <nekes125@outlook.com>
2023-07-19 21:14:56 -0600

    Update RequestSender.hpp

--- include/Server/RequestSender.hpp
+++ include/Server/RequestSender.hpp
@@ -35,7 +35,7 @@ private:
 		Time::timePoint now = Time::now();
 		size_t secs = (Time::diff(Timer::tp, now).count() / 1000);
 		if (secs % 15 == 0) {
-			std::cout << "\rAvg RPS: " << (reqs / secs);
+			std::cout << "\rAvg RPS: " << float(float(reqs) / float(secs));
 		}
 	}
 
