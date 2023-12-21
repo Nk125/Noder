97319e2d2990ffa062b42c9d64fdac66dba22980
nk125 <nekes125@outlook.com>
2023-12-21 02:17:22 -0600

    Changed CXX compiler execution

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -37,7 +37,7 @@ else
 fi
 mkdir "${buildPath}" 1>/dev/null 2>&1
 echo "Configuring"
-cmake -B "${buildPath}" -S "${sourceDir}" -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -G "Ninja" -DCMAKE_C_COMPILER="clang-12" -DCMAKE_CXX_COMPILER="clang-12"
+cmake -B "${buildPath}" -S "${sourceDir}" -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -G "Ninja" -DCMAKE_C_COMPILER="clang-12" -DCMAKE_CXX_COMPILER="clang++"
 echo "Building"
 cmake --build "${buildPath}" --target node
 echo "Built"
